c9f64cf8720688cb0e5d9a53dd04b49f7dd92e10
Oren Farhi <farhioren@gmail.com>
2014-07-22 15:37:01 +0300

    browserify: adding source maps

--- grunt/browserify.js
+++ grunt/browserify.js
@@ -6,7 +6,10 @@ module.exports = function(grunt) {
 	        	'src/bower_components/bootstrap/js/dropdown.js:bsDropdown',
 	        	'src/bower_components/react/react.js:react',
 	        	'src/bower_components/react-bootstrap:react-bootstrap'
-	        ]
+	        ],
+	        bundleOptions: {
+	        	debug: true
+	        }
 	    },
 	    client: {
 	        src: ['src/app/**/*.js'],
